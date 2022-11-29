1792893ea7c4958f8eaf901a8b9679edbdf068da
luciamartinez <luciamacarenamartinez@gmail.com>
2022-11-28 21:11:02 -0300

    starting with js

--- js/script.js
+++ js/script.js
@@ -1,162 +1,36 @@
-[
-    {
-      "name": "Ingeniero QA",
-      "description": "description 1",
-      "category": "QA",
-      "seniority": "Senior",
-      "location": "Estados Unidos",
-      "id": "1"
-    },
-    {
-      "name": "Tecnico de Infraestructura",
-      "description": "description 2",
-      "category": "Infraestructura",
-      "seniority": "Junior",
-      "location": "Chile",
-      "id": "2"
-    },
-    {
-      "name": "Desarrollador FrontEnd",
-      "description": "description 3",
-      "category": "Desarrollo",
-      "seniority": "Trainee",
-      "location": "Nueva Zelanda",
-      "id": "3"
-    },
-    {
-      "name": "DevOps",
-      "description": "description 4",
-      "category": "Seguridad",
-      "seniority": "Junior",
-      "location": "Uruguay",
-      "id": "4"
-    },
-    {
-      "name": "Desarrollador Backend",
-      "description": "description 5",
-      "category": "Desarrollo",
-      "seniority": "Trainee",
-      "location": "Brasil",
-      "id": "5"
-    },
-    {
-      "name": "Analista funcional",
-      "description": "description 6",
-      "category": "Infraestructura",
-      "seniority": "Senior",
-      "location": "Chile",
-      "id": "6"
-    },
-    {
-      "name": "Desarrollador FullStack",
-      "description": "description 7",
-      "category": "Desarrollo",
-      "seniority": "Junior",
-      "location": "Brasil",
-      "id": "7"
-    },
-    {
-      "name": "Analista de datos",
-      "description": "description 8",
-      "category": "Seguridad",
-      "seniority": "Senior",
-      "location": "Estados Unidos",
-      "id": "8"
-    },
-    {
-      "name": "Analista",
-      "description": "description 9",
-      "category": "QA",
-      "seniority": "Trainee",
-      "location": "Chile",
-      "id": "9"
-    },
-    {
-      "name": "Analista de Data",
-      "description": "description 10",
-      "category": "Infraestructura",
-      "seniority": "Senior",
-      "location": "Brasil",
-      "id": "10"
-    },
-    {
-      "name": "Desarrollador BackEnd",
-      "description": "description 11",
-      "category": "Desarrollo",
-      "seniority": "Junior",
-      "location": "España",
-      "id": "11"
-    },
-    {
-      "name": "Ingeniero QA",
-      "description": "description 12",
-      "category": "QA",
-      "seniority": "Senior",
-      "location": "Nueva Zelanda",
-      "id": "12"
-    },
-    {
-      "name": "DevOps",
-      "description": "description 13",
-      "category": "Seguridad",
-      "seniority": "Junior",
-      "location": "España",
-      "id": "13"
-    },
-    {
-      "name": "Ingeniero en Infraestructura",
-      "description": "description 14",
-      "category": "Infraestructura",
-      "seniority": "Senior",
-      "location": "Uruguay",
-      "id": "14"
-    },
-    {
-      "name": "Desarrollador BackEnd",
-      "description": "description 15",
-      "category": "Desarrollo",
-      "seniority": "Junior",
-      "location": "Chile",
-      "id": "15"
-    },
-    {
-      "name": "DevOps",
-      "description": "description 16",
-      "category": "Seguridad",
-      "seniority": "Junior",
-      "location": "España",
-      "id": "16"
-    },
-    {
-      "name": "Ingeniero QA",
-      "description": "description 17",
-      "category": "QA",
-      "seniority": "Senior",
-      "location": "Chile",
-      "id": "17"
-    },
-    {
-      "name": "Tecnico de Infraestructura",
-      "description": "description 18",
-      "category": "Infraestructura",
-      "seniority": "Trainee",
-      "location": "Uruguay",
-      "id": "18"
-    },
-    {
-      "name": "Desarrollador FullStack",
-      "description": "description 19",
-      "category": "Desarrollo",
-      "seniority": "Senior",
-      "location": "Estados Unidos",
-      "id": "19"
-    },
-    {
-      "name": "Analista de datos",
-      "description": "description 20",
-      "category": "Seguridad",
-      "seniority": "Junior",
-      "location": "Nueva Zelanda",
-      "id": "20"
-    }
-  ]
+
+// const $ = (selector) => document.querySelector(selector)
+
+// const generateCards = () => {
+// fetch(`https://63853647beaa6458265b9975.mockapi.io/Jobs`)
+// .then(res => res.json())
+// .then(data => generateCard(data))
+// .catch(() => pokemonNotFound())
+// }
+
+// const user = {
+//   name: "test EDICION PUT",
+//   age: 20,
+//   email: "lala@lala.com"
+// }
+
+// fetch(`https://63853647beaa6458265b9975.mockapi.io/Jobs`,{
+//   method: "POST",
+//   headers:{
+//     'Content-Type': 'Application/json'
+//   },
+//   body: JSON.stringify(user)
+// })
+
+// fetch(`https://63853647beaa6458265b9975.mockapi.io/Jobs/1`,{
+//   method: "PUT",
+//   headers:{
+//     'Content-Type': 'Application/json'
+//   },
+//   body: JSON.stringify(user)
+// })
+
+
+// fetch(`https://63853647beaa6458265b9975.mockapi.io/Jobs/1`,{
+//   method: "DELETE"
+// })
